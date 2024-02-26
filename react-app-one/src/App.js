4a4cf7c853baaf421bb8fc06341f1e73d6da4035
nzr.bones@gmail.com <nzr.bones@gmail.com>
2024-02-26 20:45:02 +0300

    react_day_1

---
+++ react-app-one/src/App.js
@@ -0,0 +1,12 @@
+import CostItem from "./components/CostItem";
+
+function App() {
+  return (
+    <div>
+      <h1>react</h1>
+      <CostItem></CostItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app-one/src/components/CostItem.js
@@ -0,0 +1,12 @@
+function CostItem() {
+    return (
+    <div>
+        <div>march 2021</div>
+        <div>
+            <h2>Холодильник</h2>
+            <div>$999</div>
+        </div>
+    </div>)
+}
+
+export default CostItem;
